4aa92d276b518e529d6d07bbfb4d067acb2ff8ea
Andrus Rinde <andrusrinde@users.noreply.github.com>
2022-04-08 13:32:45 +0300

    Kahe esimese tunni asjad

---
+++ add_news.php
@@ -0,0 +1,90 @@
+<?php
+	require_once "../../../cnf.php";
+	require_once "fnc_news.php";
+	//require_once "fnc_general.php";
+	//$_POST
+	//$_GET
+	//var_dump($_POST);
+	//echo $_POST["newsInput"];
+	$author_name = "Andrus Rinde";
+	
+	$news_input_error = null;
+	$notice = null;
+	$title_input = null;
+	$news_input = "";
+	$expire_input = null;
+	
+	if(isset($_POST["newsSubmit"])){
+		//kontrollime uudise sisu
+		if(isset($_POST["titleInput"]) and !empty($_POST["titleInput"])){
+			$title_input = $_POST["titleInput"];
+		} else {
+			$news_input_error = "Uudise pealkiri on puudu! ";
+		}
+		
+		if(isset($_POST["newsInput"]) and !empty($_POST["newsInput"])){
+			$news_input = $_POST["newsInput"];
+		} else {
+			$news_input_error .= "Uudise sisu on puudu! ";
+		}
+		
+		//neid peaks ka kontrollima
+		$expire_input = $_POST["expireInput"];
+		
+		$notice = $news_input_error;
+		
+		if(empty($news_input_error)){
+			$notice = save_news($title_input, $news_input, $expire_input);
+		}
+		
+	}
+	
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo $author_name; ?> teeb veebi</title>
+    <link rel="stylesheet" type="text/css" href="styles/general.css">
+</head>
+<body>
+	<header>
+		<img id="banner" src="../../media/pic/rif21_banner.png" alt="RIF21 bänner">
+		<h1><?php echo $author_name; ?> arendab veebi</h1>
+		<details>
+			<summary>Selle lehe mõte</summary>
+			<p>See leht on loodud õppetöö raames ja ei sisalda tõsiseltvõetavat materjali!</p>
+		</details>
+		
+        <hr>
+	</header>
+    
+    <nav>
+        <h2>Olulised lingid</h2>
+        <ul>
+            <li><a href="https://www.tlu.ee/haapsalu">Tallinna Ülikooli Haapsalu kolledž</a></li>
+            
+        </ul>
+    </nav>
+        
+	<main>
+		<section>
+			<h2>Lisa uudis</h2>
+			<form method="POST">
+				<label for="titleInput">Uudise pealkiri</label>
+				<input type="text" id="titleInput" name="titleInput" placeholder="Kirjuta siia pealkiri ...">
+				<br>
+				<label for="newsInput">Uudise tekst</label><br>
+				<textarea id="newsInput" name="newsInput" cols="60" rows="5" placeholder="Kirjuta siia uudise tekst ..."></textarea>
+				<br>
+				<label for="expireInput">Uudise aegumistähtaeg</label>
+				<input type="date" id="expireInput" name="expireInput">
+				<br>
+				<input type="submit" id="newsSubmit" name="newsSubmit" value="Salvesta uudis">
+			</form>
+			<?php echo "<p>" .$notice ."</p> \n"; ?>
+		</section>
+<?php
+	require_once "pagefooter.php";
+?>
---
+++ page.php
@@ -0,0 +1,126 @@
+<?php
+	require_once "fnc_general.php";
+	
+	$author_name = "Andrus Rinde";
+	
+	//piltide kataloog
+	$photo_dir = "hkphotos/";
+	$all_files = read_dir_content($photo_dir);
+	//var_dump($all_files);
+	$allowed_photo_types = ["image/jpeg", "image/png"];
+	$photo_files = check_if_photo($all_files, $photo_dir, $allowed_photo_types);
+	//näitame kolme juhuslikku fotot!
+	$photo_count = count($photo_files);
+	$photo_count_limit = 3;
+	if($photo_count < 3){
+		$photo_count_limit = $photo_count;
+	}
+	$random_photo_files = array_rand($photo_files, $photo_count_limit);
+	//echo $photo_count;
+	//$random_num = mt_rand(0, $photo_count - 1);
+	//$photo_html = "\n" .'<img src="' .$photo_dir .$photo_files[$random_num] .'" alt="Haapsalu kolledž"  class="photoframe">' ."\n";
+	$photo_html = "";
+	foreach ($random_photo_files as $file){
+	$photo_html .= "\n" .'<img src="' .$photo_dir .$photo_files[$file] .'" alt="Haapsalu kolledž"  class="photoframe">' ."\n";
+	}
+	
+	
+	$full_time_now = date("d.m.Y H:i:s");
+	$weekday_now = date("N");
+	//echo $weekday_now;
+	$weekday_names_et = ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev", "pühapäev"];
+	//echo $weekday_names_et[$weekday_now - 1];
+	$day_category = "lihtsalt päev";
+	if($weekday_now <= 5){
+		$day_category = "kooli- või tööpäev";
+	} else {
+		$day_category = "normaalsete inimeste puhkepäev";
+	}
+	
+	$part_of_day = "hägune aeg";
+	$hour_now = date("H");
+	if($hour_now < 6 or $hour_now >= 23){
+			$part_of_day = "uneaeg";
+		}
+		if($hour_now >= 6 and $hour_now < 8){
+			$part_of_day = "hommik";
+		}
+		if($hour_now >= 8 and $hour_now < 18){
+			$part_of_day = "aktiivne aeg";
+		}
+		if($hour_now >= 18 and $hour_now < 23){
+			$part_of_day = "õhtu";
+		}
+	
+	$semester_begin = new DateTime("2022-1-31");
+	$semester_end = new DateTime("2022-6-30");
+	$semester_duration = $semester_begin->diff($semester_end);
+	//echo $semester_duration;
+	$semester_duration_days = $semester_duration->format("%r%a");
+	//echo $semester_duration_days;
+	$from_semester_begin = $semester_begin->diff(new DateTime("now"));
+	$from_semester_begin_days = $from_semester_begin->format("%r%a");
+	
+	if($from_semester_begin_days > 0){
+		if($from_semester_begin_days <= $semester_duration_days){
+			$semester_meter = "\n" .'<p>Semester edeneb: <meter min="0" max="' .$semester_duration_days .'" value="' .$from_semester_begin_days .'"></meter>.</p>' ."\n";
+		} else {
+			$semester_meter = "\n <p>Semester on lõppenud!</p> \n";
+		}
+	} elseif($from_semester_begin_days === 0) {
+		$semester_meter = "\n <p>Semester algab täna!</p> \n";
+	} else {
+		$semester_meter = "\n <p>Semestri alguseni on jäänud " . (abs($from_semester_begin_days) + 1) ." päeva!</p> \n";
+	}
+	
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo $author_name; ?> teeb veebi</title>
+    <link rel="stylesheet" type="text/css" href="styles/general.css">
+</head>
+<body>
+	<header>
+		<img id="banner" src="../../media/pic/rif21_banner.png" alt="RIF21 bänner">
+		<h1><?php echo $author_name; ?> arendab veebi</h1>
+		<details>
+			<summary>Selle lehe mõte</summary>
+			<p>See leht on loodud õppetöö raames ja ei sisalda tõsiseltvõetavat materjali!</p>
+		</details>
+		
+        <hr>
+	</header>
+    
+    <nav>
+        <h2>Olulised lingid</h2>
+        <ul>
+            <li><a href="https://www.tlu.ee/haapsalu">Tallinna Ülikooli Haapsalu kolledž</a></li>
+            
+        </ul>
+    </nav>
+        
+	<main>
+		<section>
+			<h2>Natuke aja kohta</h2>
+			<p>Lehe avamise hetk: <?php echo $weekday_names_et[$weekday_now - 1] .", " .$full_time_now .", on " .$day_category. ", " .$part_of_day ."."; ?></p>
+			<?php echo $semester_meter; ?>
+			
+		</section>
+		<section>
+			<h2>Vaated Haapsalu kolledžile</h2>
+			<?php echo $photo_html; ?>
+			<figure>
+				<img src="../../media/photos/HK_600x400/IMG_3238.JPG" alt="TLÜ Haapsalu kolledži hoone" class="photoframe">
+				<figcaption>Vaade TLÜ Haapsalu kolledži hoonele Lihula poolt</figcaption>
+			</figure>
+			<figure>
+				<img src="../../media/photos/HK_600x400/IMG_4761.JPG" alt="TLÜ Haapslau kolledži arvutiklass 205" class="photoframe">
+				<figcaption>Vaade TLÜ Haapsalu kolledži arvutiklassi</figcaption>
+			</figure>
+		</section>
+<?php
+	require_once "pagefooter.php";
+?>
